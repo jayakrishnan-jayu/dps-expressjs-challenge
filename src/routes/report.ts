4a5e0d5a6913aef0b329bedddac66bc1c5899f01
jayakrishnan-jayu <jayakrishnan-jayu@protonmail.com>
2025-05-12 10:49:41 +0200

    fix: update report routes to use service layer logic

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -1,33 +1,22 @@
 import express, { Request, Response } from 'express';
+import { reportSvc } from '../services';
 
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-	res.send('all reports');
-});
+router.route('/').get(reportSvc.getAll).post(reportSvc.create);
+
+router
+	.route('/:id')
+	.get(reportSvc.byID)
+	.put(reportSvc.update)
+	.delete(reportSvc.remove);
 
 router.get('/special-report', (req: Request, res: Response) => {
 	res.send('speical report');
 });
 
-router.get('/:id', (req: Request, res: Response) => {
-	res.send(`report with id ${req.params['id']}`);
-});
-
 router.get('/:project-id', (req: Request, res: Response) => {
 	res.send(`reports with id ${req.params['project-id']}`);
-});
-
-router.post('/', (req: Request, res: Response) => {
-	res.send('create report');
-});
-
-router.put('/:id', (req: Request, res: Response) => {
-	res.send(`update report ${req.params['id']}`);
-});
-
-router.delete('/:id', (req: Request, res: Response) => {
-	res.send(`delete report with id ${req.params['id']}`);
 });
 
 export default router;
